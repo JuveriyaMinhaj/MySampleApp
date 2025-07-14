32b4467d9243f21b1fe911d148d1772d0609ea96
Majoju-Nandhini <nandhinimajoju@gmail.com>
2025-07-15 03:57:49 +0530

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def firstApp():
-    return "Hello Worlddd welcome"
+    return "Hello"
 if __name__ == "__main__":
     app.run(debug = True)
